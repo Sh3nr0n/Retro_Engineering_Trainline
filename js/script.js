894ca250955827ff7dd44edaee58711a23fc845d
Sh3nr0n <mehdi.lefeuvre@gmail.com>
2018-03-19 15:54:35 +0100

    form and sponsors done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
 $(document).ready(function () {
-    $(".userInput").on("click", function (){
-        $('#mainModal').modal('show');
-        nom = $(this).val();
-        // console.log("nom hotel : ",nom);
-        $("#hotel").text(nom);
-    });
+    
 });
